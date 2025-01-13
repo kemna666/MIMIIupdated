2f8848e06efd3682fbc9c886ac11dcdbbfec976d
kemna <kemna666@gmail.com>
2025-01-13 18:24:32 +0800

    增加了准确率

--- modified.py
+++ modified.py
@@ -42,6 +42,7 @@ class CNN(nn.Module):
         self.Conv2 = nn.Conv1d(hidden_dim, output_dim, kernel_size=3, stride=1, padding=1)
         self.pool = nn.MaxPool1d(kernel_size=3, stride=2)
         self.relu = nn.ReLU()
+        self.dropout = nn.Dropout(p=0.5)
         conv1_out_length = (length + 2*1 - 7) // 1 + 1
         pool1_out_length = (conv1_out_length - 3) // 2 + 1
         conv2_out_length = (pool1_out_length + 2*1 - 3) // 1 + 1
@@ -59,9 +60,11 @@ class CNN(nn.Module):
         x = self.Conv1(x)
         x = self.relu(x)
         x = self.pool(x)
+        x = self.dropout(x)
         x = self.Conv2(x)
         x = self.relu(x)
         x = self.pool(x)
+        x = self.dropout(x) 
         x = x.view(x.size(0), -1)
         x = self.fc(x)
         return x  # 只返回标签
